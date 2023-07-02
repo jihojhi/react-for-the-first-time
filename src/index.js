cd0302a4b15b0c6b12b65f0fdb727be9628f43ca
hyojin <jihojhi@gmail.com>
2023-07-02 20:28:34 +0900

    :technologist: Event & Event Handler Study

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// import NotificationList from './chapter_06/NotificationList';
-import Accommodate from "./chapter_07/Accommodate";
+import ConfirmButton from "./chapter_08/ConfirmButton"
+import ConfirmButtonFunc from "./chapter_08/ConfirmButtonFunc";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Accommodate />
+    <ConfirmButtonFunc />
   </React.StrictMode>
 );
 
